2632593f681558e1f0ac2e7e89874ed971874955
askayahia <aska.yahia@gmail.com>
2023-10-31 17:10:02 -0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-0x00-python-hello world
+0x00-python-hello world by Yahia
--- lists.h
+++ lists.h
@@ -4,9 +4,9 @@
 #include <stdlib.h>
 
 /**
- * struct listint_s - singly linked list
+ * struct listint_s - A singly linked list
  * @n: integer
- * @next: points to the next node
+ * @next: Point to the next node
  *
  * Description: singly linked list node structure
  *
